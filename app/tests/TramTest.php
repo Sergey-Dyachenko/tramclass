610e0c8e1e819e3028103d1678d1fe9333dcbb7a
sergey <serjdyachenko@gmail.com>
2019-04-25 09:26:01 +0300

    first commit

---
+++ app/tests/TramTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 24.04.2019
+ * Time: 22:21
+ */
+
+namespace app\tests\TramTest;
+
+class TramTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetAvaragePassengerWeight(){
+        return true;
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+use app\model\Tram\Tram;
+
+$tram = new Tram(2000, 80, 30, 60 );
